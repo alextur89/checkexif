3bb085b2d228cd7be8c4d1636432585baac88753
Alexander Tyuryuchkin <tyuryuchkin@gmail.com>
2019-05-04 23:46:24 +0300

    feat Add setup file

---
+++ check_exif/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='CheckEXIF',
+    version='0.1dev',
+    py_modules=['check_exif'],
+    license='MIT',
+    author='Alexander Tyuryuchkin',
+    author_email='tyuryuchkin@gmail.com',
+)
