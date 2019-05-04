2425634b9daa74b67cecbef881108c0312d7ddca
Alexander Tyuryuchkin <tyuryuchkin@gmail.com>
2019-05-05 00:02:55 +0300

    ref Refactor project structure

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='CheckEXIF',
     version='0.1dev',
-    py_modules=['check_exif'],
+    py_modules=['check_exif/check_exif'],
     license='MIT',
     author='Alexander Tyuryuchkin',
     author_email='tyuryuchkin@gmail.com',
