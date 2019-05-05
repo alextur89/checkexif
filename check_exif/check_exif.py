ba3f57bc08c6f7b306146b084739b7aca9738832
Alexander Tyuryuchkin <tyuryuchkin@gmail.com>
2019-05-05 11:41:24 +0300

    feat Add show option to show date in file

--- check_exif/check_exif.py
+++ check_exif/check_exif.py
@@ -9,17 +9,12 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='Change date in image metadata.')
     parser.add_argument('--filename', '-f', dest='filename', type=open, help='File to be change', nargs='+', required=True)
-    parser.add_argument('--date', '-d', dest='date', help='New date, current date for default')
+    parser.add_argument('--date', '-d', dest='date', help='New date')
+    parser.add_argument('--show', '-s', action='store_true', help='Show date')
 
     args = parser.parse_args()
 
     for fname in args.filename:
-        if args.date:
-            todate = str(args.date)
-        else:
-            currentDT = datetime.datetime.now()
-            todate=str(currentDT)
-
         try:
             img = Image.open(fname.name)
         except IOError:
@@ -28,12 +23,19 @@ if __name__ == '__main__':
 
         exif_dict = piexif.load(img.info['exif'])
 
-        try:
-            exif_dict['Exif'][36867] = todate
-            exif_dict['Exif'][36868] = todate
-            exif_dict['0th'][306] = todate
-        except KeyError:
-            print ("Error in file's metadata")
-            sys.exit(-1)
+        if args.show:
+            print ("Initial date: " + str(exif_dict['Exif'][36867]) )
+            print ("Date of digitization: " + str(exif_dict['Exif'][36868]) )
+            print ("Date: " + str(exif_dict['0th'][306]))
 
-        piexif.insert(piexif.dump(exif_dict), fname.name)
+        if args.date:
+            todate = str(args.date)
+            try:
+                exif_dict['Exif'][36867] = todate
+                exif_dict['Exif'][36868] = todate
+                exif_dict['0th'][306] = todate
+            except KeyError:
+                print ("Error in file's metadata")
+                sys.exit(-1)
+
+            piexif.insert(piexif.dump(exif_dict), fname.name)
